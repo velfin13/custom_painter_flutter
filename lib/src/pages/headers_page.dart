efa68c1f4ee181746f66dad9781f5145bd2013c2
Velfin Velasquez <velfinvelasquez@gmail.com>
2022-11-19 10:58:38 -0500

    header cuadrado

---
+++ lib/src/pages/headers_page.dart
@@ -0,0 +1,11 @@
+import 'package:custom_painter/src/widgets/headerCuadrado.dart';
+import 'package:flutter/material.dart';
+
+class HeaderPage extends StatelessWidget {
+  const HeaderPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: HeaderCuadrado());
+  }
+}
---
+++ lib/src/widgets/headerCuadrado.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HeaderCuadrado extends StatelessWidget {
+  const HeaderCuadrado({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      color: const Color(0xff615aab),
+    );
+  }
+}
